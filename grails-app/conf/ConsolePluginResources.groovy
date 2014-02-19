e1e824afa5ca6f8f4a29d773b203662652e16e54
domurtag <domurtag@yahoo.co.uk>
2014-02-19 16:53:42 +0000

    fixes GPCONSOLE-10 - resources are missing content type

--- grails-app/conf/ConsolePluginResources.groovy
+++ grails-app/conf/ConsolePluginResources.groovy
@@ -2,15 +2,15 @@ modules = {
 
 	'console' {
 
-		resource url: [dir: 'js/CodeMirror-2.23/lib', file: 'codemirror.css', plugin: 'console']
-		resource url: [dir: 'css', file: 'jquery.layout.css', plugin: 'console']
-		resource url: [dir: 'css', file: 'grails-console.css', plugin: 'console']
+		resource url: [dir: 'js/CodeMirror-2.23/lib', file: 'codemirror.css', plugin: 'console'], attrs: [type: 'css']
+		resource url: [dir: 'css', file: 'jquery.layout.css', plugin: 'console'], attrs: [type: 'css']
+		resource url: [dir: 'css', file: 'grails-console.css', plugin: 'console'], attrs: [type: 'css']
 
 		for (name in ['jquery-1.7.1.min', 'jquery-ui-1.8.17.custom.min',
 		              'jquery.layout', 'jquery.Storage', 'jquery.hotkeys',
 		              'CodeMirror-2.23/lib/codemirror', 'CodeMirror-2.23/mode/groovy/groovy',
 		              'grails-console/console']) {
-			resource url: [dir: 'js', file: name + '.js', plugin: 'console']
+			resource url: [dir: 'js', file: name + '.js', plugin: 'console'], attrs: [type: 'js']
 		}
 	}
 }
